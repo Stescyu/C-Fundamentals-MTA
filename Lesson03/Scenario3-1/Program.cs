80920c2cbf47a6076b62ddd7e6f157b5a00fe4ae
Stescyu <stevenyusc@gmail.com>
2017-12-01 13:49:26 -0500

    Lesson03 MTA Tutorial Added

---
+++ Lesson03/Scenario3-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Scenario3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { 1, 2, 3, 4, 5 }, { 3, 4, 5, 6, 7 }, { 5, 6, 7, 8, 9 }, { 6, 7, 8, 9, 10 } };
+            int maxNum = 0;
+            for(int i = 0; i < 4; i++){
+                for (int j = 0; j < 5; j++){
+                    if(arr[i,j] > maxNum){
+                        maxNum = arr[i,j];
+                    }
+                }
+                Console.WriteLine(maxNum);
+            }
+        }
+    }
+}
